88d30edcc7c64335fbc1a0b2a6b2d0eab8713695
Jostein Gogstad <jostein.gogstad@gmail.com>
2020-01-03 09:38:42 +0100

    Use alias for cat instead of symlink

--- bat.plugin.zsh
+++ bat.plugin.zsh
@@ -2,10 +2,7 @@ if type bat > /dev/null; then
   export MANPAGER="sh -c 'col -bx | bat -l man -p'"
   export BAT_STYLE=plain
 
-  ! [[ "$PATH" = *"$HOME/bin"* ]] && echo "Warning: Placing symlink for bat in $HOME/bin, but directory not found on PATH" >&2
-  
-  mkdir -p "$HOME/bin"
-  ! [[ -f "$HOME/bin/cat" ]] && ln -s "$(command -v bat)" "$HOME/bin/cat"
+  alias cat='bat --pager=never'
 else
   echo "Error: Please install bat before loading ZSH plugin" >&2
 fi
